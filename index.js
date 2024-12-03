14dad692f9019e0cbfb8b16e2a65dc5546db101e
Berkay Alkan <berky.alknn@gmail.com>
2024-12-03 22:22:46 +0300

    add conditional logic to ensure measures are displayed ony if they exist

--- index.js
+++ index.js
@@ -3,15 +3,17 @@ import axios from "axios";
 
 
 const app = express();
-const port = 3000;
-const API_URL = "https://www.thecocktaildb.com/api/json/v1/1/random.php"
+const port = 4000;
+const API_URL = "https://www.thecocktaildb.com/api/json/v1/1/random.php";
 app.use(express.static("public"));
+app.set("view engine", "ejs");
 
 app.get("/", async (req, res) => {
+    
     try {
         const result = await axios.get(API_URL); 
         const cocktail = result.data.drinks[0]; 
-        res.render("index.ejs",
+        res.render("index",
          { 
             name : cocktail.strDrink,
             id : cocktail.idDrink,
@@ -25,11 +27,17 @@ app.get("/", async (req, res) => {
             i4 : cocktail.strIngredient4,
             i5 : cocktail.strIngredient5,
             i6: cocktail.strIngredient6,
-            strDrinkThumb: cocktail.strDrinkThumb
+            strDrinkThumb: cocktail.strDrinkThumb,
+            measure1 : cocktail.strMeasure1,
+            measure2 : cocktail.strMeasure2,
+            measure3 : cocktail.strMeasure3,
+            measure4 : cocktail.strMeasure4,
+            measure5 : cocktail.strMeasure5,
+            measure6 : cocktail.strMeasure6,
          });  
     } catch (error) {
-        console.log(error.response ? error.response.data : error.message);
-        res.status(500).send("Something went wrong with the API call.");
+        console.error(error);
+        res.status(500).send("Something went wrong while rendering the page.");
     }
 });
 
